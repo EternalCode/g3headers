331e42cd5eb11c113f1dce29b4504d7f2792d3a7
Evan <eroelke@gmail.com>
2019-03-04 15:49:20 -0700

    brm data struct for relevant data

--- include/pokeagb/brm/pokemenu.h
+++ include/pokeagb/brm/pokemenu.h
@@ -10,6 +10,25 @@ POKEAGB_BEGIN_DECL
 
 
 /**
+ * BRM data structure
+ * struct is larger than this, rest is unknown though
+ */
+struct gBrmData {
+	/*0x00*/ u32 brmCallback;
+	/*0x04*/ u16 field4;
+	/*0x06*/ u16 field6;
+	/*0x08*/ u16 field8;
+	/*0x0A*/ u16 fieldA;
+	/*0x0C*/ u8 optsBoxId;   // box that holds brm options
+	/*0x0D*/ u8 actionBoxId;  // box in bottom left that tells you what to do
+	/*0x0E*/ u8 fieldMoveHelpboxId;	// lists what field move does
+	/*0x0F*/ u8 brmSequence[8];	// sequence of brm Ids
+	/*0x17*/ u8 numBrmOpts;		// number of brm options
+};
+
+ASSERT_SIZEOF(struct gBrmData, 0x18);
+
+/**
  * BRM address
  * @address{BPRE,0203b0a0}
  */
@@ -19,7 +38,7 @@ extern u32 gAddrBRM;
  * BRM data pointer
  * @address{BPRE,0203b09c}
  */
-extern u32 gPtrBrmData;
+extern struct gBrmData* gPtrBrmData;
 
 
 /**
@@ -161,6 +180,115 @@ POKEAGB_EXTERN void BRMStringBufferTask(u8 taskId);
  * @address{BPRE,080e56dc}
  */
 POKEAGB_EXTERN void TaskSelectBRMPokeForHeal(u8 taskId);
+
+
+/**
+ * task for summary screen from BRM
+ * @address{BPRE,08122d4c}
+ */
+POKEAGB_EXTERN void brmLaunchSummary(u8 taskId);
+
+/**
+ * task for switching pokes in BRM
+ * @address{BPRE,08122E00}
+ */
+POKEAGB_EXTERN void brmLaunchSwitch(u8 taskId);
+
+/**
+ * task for canceling BRM
+ * @address{BPRE,08123640}
+ */
+POKEAGB_EXTERN void brmLaunchCancel(u8 taskId);
+
+/**
+ * task for making sub brm menu for Item
+ * @address{BPRE,081236A0}
+ */
+POKEAGB_EXTERN void brmLaunchItemMenu(u8 taskId);
+
+/**
+ * task for giving item from BRM
+ * @address{BPRE,08123704}
+ */
+POKEAGB_EXTERN void brmLaunchGiveItem(u8 taskId);
+
+/**
+ * task for taking item from BRM
+ * @address{BPRE,08123C38}
+ */
+POKEAGB_EXTERN void brmLaunchTakeItem(u8 taskId);
+
+/**
+ * task for making Mail sub menu in BRM
+ * @address{BPRE,08123CF4}
+ */
+POKEAGB_EXTERN void brmLaunchMailMenu(u8 taskId);
+
+/**
+ * task for taking mail in BRM
+ * @address{BPRE,08123E0C}
+ */
+POKEAGB_EXTERN void brmLaunchTakeMail(u8 taskId);
+
+/**
+ * task for reading mail in BRM
+ * @address{BPRE,08123D58}
+ */
+POKEAGB_EXTERN void brmLaunchReadMail(u8 taskId);
+
+/**
+ * task for cancel sub menu in BRM
+ * @address{BPRE,08124064}
+ */
+POKEAGB_EXTERN void brmLaunchCancelSubBrmMenu(u8 taskId);
+
+/**
+ * task for shifting/sending out poke
+ * @address{BPRE,081240F4}
+ */
+POKEAGB_EXTERN void brmLaunchShiftOrSendOut(u8 taskId);
+
+/**
+ * task for entering poke in something
+ * @address{BPRE,08124154}
+ */
+POKEAGB_EXTERN void brmLaunchEnter(u8 taskId);
+
+/**
+ * task for being unable to enter poke in something
+ * @address{BPRE,08124278}
+ */
+POKEAGB_EXTERN void brmLaunchNoEntry(u8 taskId);
+
+/**
+ * task for storing pokemon from BRM
+ * @address{BPRE,08124354}
+ */
+POKEAGB_EXTERN void brmLaunchStore(u8 taskId);
+
+/**
+ * task for registering pokemon from BRM
+ * @address{BPRE,08124384}
+ */
+POKEAGB_EXTERN void brmLaunchRegister(u8 taskId);
+
+/**
+ * task for trading pokemon from BRM
+ * @address{BPRE,08124490}
+ */
+POKEAGB_EXTERN void brmLaunchTrade(u8 taskId);
+
+/**
+ * extra trade func in BRM... does nothing
+ * @address{BPRE,081245A0}
+ */
+POKEAGB_EXTERN void brmLaunchNull(u8 taskId);
+
+/**
+ * launch field move from BRM
+ * @address{BPRE,081245A4}
+ */
+POKEAGB_EXTERN void brmLaunchFieldMove(u8 taskId);
 
 
 
